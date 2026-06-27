84b9fe87cc87b210dbb059b3c977dc825ba6c241
unknown <tobias.vida@live.com>
2026-06-27 02:54:46 -0300

    feat: switch direction

--- Assets/Scripts/MovingPlataform.cs
+++ Assets/Scripts/MovingPlataform.cs
@@ -5,6 +5,10 @@ public class MovingPlatform : MonoBehaviour
     public float speed = 2f;
     public float distance = 3f;
 
+    [Header("Configurações de Movimento")]
+    [Tooltip("Defina a direção do movimento. Ex: X=1, Y=0 (Direita) | X=0, Y=1 (Cima)")]
+    public Vector3 moveDirection = Vector3.right;
+
     private Vector3 startPos;
     private Vector3 lastPos;
     private int direction = 1;
@@ -15,12 +19,16 @@ public class MovingPlatform : MonoBehaviour
     {
         startPos = transform.position;
         lastPos = transform.position;
+
+        // Normalizamos o vetor para garantir que a velocidade seja constante, 
+        // independente dos números que você colocar no Inspector
+        moveDirection = moveDirection.normalized;
     }
 
     void FixedUpdate()
     {
-        // Atualiza a posição manualmente (Modo cinemático)
-        transform.position += Vector3.right * direction * speed * Time.fixedDeltaTime;
+        // Agora usamos o moveDirection no lugar do Vector3.right
+        transform.position += moveDirection * direction * speed * Time.fixedDeltaTime;
 
         if (Vector3.Distance(startPos, transform.position) >= distance)
         {
